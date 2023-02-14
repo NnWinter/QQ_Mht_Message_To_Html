421538ef314ff33c5be79826b3072dbbf1a514f9
NnWinter <515621078@163.com>
2023-02-15 03:08:23 +0800

    发布配置

--- v3.1/obj/Debug/net6.0-windows/v3.1.AssemblyInfo.cs
+++ v3.1/obj/Debug/net6.0-windows/v3.1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("v3.1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Nn - QQ消息 mht 转 html")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("v3.1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("v3.1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Nn - QQ消息 mht 转 html")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Nn - QQ消息 mht 转 html")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
